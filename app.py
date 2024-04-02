93ef6225f0c52603dcfbf9f0d958690814ecd509
Vishal Hirawat <hirawatt@pm.me>
2024-04-02 11:02:36 +0530

    UI changes

--- app.py
+++ app.py
@@ -11,7 +11,6 @@ import pickle
 
 def get_user_input():
     numeric_values = [st.sidebar.number_input(f'Enter {col}') for col in ['X', 'Y', 'CENSUS_TRC', 'OCC_DAY', 'OCC_MONTH', 'OCC_YEAR']]
-    
     division = st.selectbox('DIVISION', ['Select Division','Central', 'East', 'West'])
     sector_formatted = st.selectbox('SECTOR_FORMATTED', ["Select Sector",'Sector 24','Sector 23',"Sector 21","Sector 25","Sector 31","Sector 13","Sector 14","Sector 21","Sector 32","Sector 22","Sector 33","Sector 34","Sector 35","Sector 17","Sector 16","Sector 15","Sector 36","Sector 12","Sector 11","Sector 37"])
     
@@ -27,24 +26,30 @@ def get_user_input():
     }
 
 def main():
-    #st.title("Random Forest Model Prediction")
-    user_input = get_user_input()
-    if st.button('Predict'):
-        # Convert user input to DataFrame if your model expects it
-        user_input_df = pd.DataFrame([user_input])
-        
-        # Use the pipeline to preprocess input and make prediction
-        prediction = loaded_model.predict(user_input_df)
-        st.write(f'Prediction: {prediction}')
-
-if __name__ == '__main__':
     st.set_page_config("Crime Location Predictor",
                         layout="wide",
                         initial_sidebar_state="expanded"
                         )
     st.sidebar.title("Crime Location Predictor")
     st.sidebar.image("./content/OPS.webp")
-    c1, c2, c3 = st.columns([4, 4, 1])
+    c1, c2, c3 = st.columns([2, 3, 1])
+    c2.video("https://youtu.be/3boD9s0oDck")
     c3.image("./content/OPS.gif", width=150)
     st.title("Crime Location Predictor")
+
+    with st.expander("About the app"):
+        st.write("Crime Location Predictor")
+    tab1, tab2, tab3 = st.tabs(["Assaults", "Theft Under 5000", "Bike Theft"])
+
+    with tab1:
+        user_input = get_user_input()
+        if st.button('Predict'):
+            # Convert user input to DataFrame if your model expects it
+            user_input_df = pd.DataFrame([user_input])
+            
+            # Use the pipeline to preprocess input and make prediction
+            prediction = loaded_model.predict(user_input_df)
+            st.write(f'Prediction: {prediction}')
+
+if __name__ == '__main__':
     main()
