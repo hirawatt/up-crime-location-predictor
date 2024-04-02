dd5c7e4716d07af5b34fc825767b01a435383540
Vishal Hirawat <hirawatt@pm.me>
2024-04-02 12:36:32 +0530

    sidebar changes

--- app.py
+++ app.py
@@ -9,13 +9,14 @@ import pickle
 #with open("./models/random_forest_model.pkl",'rb') as file:
     #loaded_model=pickle.load(file)
 
-def get_user_input():
-    numeric_values = [st.sidebar.number_input(f'Enter {col}') for col in ['X', 'Y', 'CENSUS_TRC', 'OCC_DAY', 'OCC_MONTH', 'OCC_YEAR']]
-    division = st.selectbox('DIVISION', ['Select Division','Central', 'East', 'West'])
-    sector_formatted = st.selectbox('SECTOR_FORMATTED', ["Select Sector",'Sector 24','Sector 23',"Sector 21","Sector 25","Sector 31","Sector 13","Sector 14","Sector 21","Sector 32","Sector 22","Sector 33","Sector 34","Sector 35","Sector 17","Sector 16","Sector 15","Sector 36","Sector 12","Sector 11","Sector 37"])
-    
-    return {
-        'SECTOR_FORMATTED': sector_formatted, 
+def get_user_input(tab):
+    if (tab==1):
+        numeric_values = [st.sidebar.number_input(f'Enter {col}', key=f"nv{tab}{col}") for col in ['X', 'Y', 'CENSUS_TRC', 'OCC_DAY', 'OCC_MONTH', 'OCC_YEAR']]
+        division = st.sidebar.selectbox('DIVISION', ['Select Division','Central', 'East', 'West'], key=f"division{tab}")
+        sector = st.sidebar.selectbox('SECTOR_FORMATTED', ["Select Sector",'Sector 24','Sector 23',"Sector 21","Sector 25","Sector 31","Sector 13","Sector 14","Sector 21","Sector 32","Sector 22","Sector 33","Sector 34","Sector 35","Sector 17","Sector 16","Sector 15","Sector 36","Sector 12","Sector 11","Sector 37"], key=f"sector{tab}")
+        st.sidebar.divider()
+        return {
+        'SECTOR_FORMATTED': sector, 
         'DIVISION': division,
         'X': numeric_values[0],
         'Y': numeric_values[1],
@@ -23,7 +24,47 @@ def get_user_input():
         'OCC_DAY': numeric_values[3],
         'OCC_MONTH': numeric_values[4],
         'OCC_YEAR': numeric_values[5]
-    }
+        }
+
+    elif (tab==2):
+        numeric_values = [st.sidebar.number_input(f'Enter {col}', key=f"nv{tab}{col}") for col in ['X', 'Y', 'CENSUS_TRC', 'OCC_DAY', 'OCC_MONTH', 'OCC_YEAR']]
+        division = st.sidebar.selectbox('DIVISION', ['Select Division','Central', 'East', 'West'], key=f"division{tab}")
+        sector = st.sidebar.selectbox('SECTOR_FORMATTED', ["Select Sector",'Sector 24','Sector 23',"Sector 21","Sector 25","Sector 31","Sector 13","Sector 14","Sector 21","Sector 32","Sector 22","Sector 33","Sector 34","Sector 35","Sector 17","Sector 16","Sector 15","Sector 36","Sector 12","Sector 11","Sector 37"], key=f"sector{tab}")
+        st.sidebar.divider()
+        return {
+            'SECTOR_FORMATTED': sector, 
+            'DIVISION': division,
+            'X': numeric_values[0],
+            'Y': numeric_values[1],
+            'CENSUS_TRC': numeric_values[2],
+            'OCC_DAY': numeric_values[3],
+            'OCC_MONTH': numeric_values[4],
+            'OCC_YEAR': numeric_values[5]
+        }
+    elif (tab==3):
+        numeric_values = [st.sidebar.number_input(f'Enter {col}', key=f"nv{tab}{col}") for col in ['X', 'Y', 'CENSUS_TRC', 'OCC_DAY', 'OCC_MONTH', 'OCC_YEAR']]
+        division = st.sidebar.selectbox('DIVISION', ['Select Division','Central', 'East', 'West'], key=f"division{tab}")
+        sector = st.sidebar.selectbox('SECTOR_FORMATTED', ["Select Sector",'Sector 24','Sector 23',"Sector 21","Sector 25","Sector 31","Sector 13","Sector 14","Sector 21","Sector 32","Sector 22","Sector 33","Sector 34","Sector 35","Sector 17","Sector 16","Sector 15","Sector 36","Sector 12","Sector 11","Sector 37"], key=f"sector{tab}")
+
+        bike_style = st.sidebar.selectbox('BIKE_STYLE', ['STOLEN','RECOVERED', 'FOUND', 'SEIZED', 'LOST','COUNTERFEIT' ])
+        bike_type = st.sidebar.selectbox('BIKE_TYPE', ['HYBRID','MOUNTAIN', 'REGULAR', 'OTHER', 'TOURING','BMX','RACER', ])
+        bike_frame = st.sidebar.selectbox('BIKE_FRAME', ["MEN'S","WOMEN'S", 'UNISEX', "CHILD'S"])
+        st.sidebar.divider()
+        return {
+            'SECTOR': sector,
+            'BIKE_STYLE': bike_style,
+            'BIKE_TYPE': bike_type,
+            'BIKE_FRAME': bike_frame,
+            'DIVISION': division,
+            'X': numeric_values[0],
+            'Y': numeric_values[1],
+            'CENSUS_TRC': numeric_values[2],
+            'OCC_DAY': numeric_values[3],
+            'OCC_MONTH': numeric_values[4],
+            'OCC_YEAR': numeric_values[5]
+        }
+    else:
+        st.error("Error selecting tab")
 
 def main():
     st.set_page_config("Crime Location Predictor",
@@ -42,7 +83,7 @@ def main():
     tab1, tab2, tab3 = st.tabs(["Assaults", "Theft Under 5000", "Bike Theft"])
 
     with tab1:
-        user_input = get_user_input()
+        user_input = get_user_input(1)
         if st.button('Predict'):
             # Convert user input to DataFrame if your model expects it
             user_input_df = pd.DataFrame([user_input])
@@ -50,6 +91,12 @@ def main():
             # Use the pipeline to preprocess input and make prediction
             prediction = loaded_model.predict(user_input_df)
             st.write(f'Prediction: {prediction}')
+    
+    with tab2:
+        user_input = get_user_input(2)
+
+    with tab3:
+        user_input = get_user_input(3)
 
 if __name__ == '__main__':
     main()
